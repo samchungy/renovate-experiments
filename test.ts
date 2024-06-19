781deb518b7c4c2118594049f5469fb2c2bec9b7
samchungy <samchungy@gmail.com>
2024-06-19 21:00:47 +1000

    test

--- test.ts
+++ test.ts
@@ -4,6 +4,5 @@ const datadog = new Datadog(this, 'datadog', {
   enableDatadogLogs: false,
   flushMetricsToLogs: false,
   extensionLayerVersion: 58,
-  // renovate: depName=DataDog/datadog-lambda-js
   nodeLayerVersion: 111,
 });
